c6ab119c906686e6d3578af9a5fcbedecdb65b0f
yoshuadupin <djosue@unitec.edu>
2017-03-05 11:38:44 -0600

    Includes del main

--- sistema.cpp
+++ sistema.cpp
@@ -1,6 +1,27 @@
+//Ventas
+#include "Sale.h"
+//Juegos
 #include "game.h"
 #include "microsoft.h"
 #include "sony.h"
+#include "bandai.h"
+#include "electronicArts.h"
+#include "konami.h"
+#include "nintendo.h"
+#include "sega.h"
+#include "squareEnix.h"
+#include "ubisoft.h"
+//Consolas
+#include "Console.h"
+#include "Microsoft.h"
+#include "Nintendo.h"
+#include "Sony.h"
+//Usuarios
+#include "user.h"
+#include "userAdmin.h"
+#include "userSeller.h"
+
+using namespace std;
 
 int main(int argc, char const *argv[]) {
 	return 0;
