510302db8f93daf3fec21b49af3c699910ee00b4
ilich-garcia <ilich-garcia@unitec.edu>
2017-03-07 22:07:14 -0600

    Revisando user.cpp

--- user.cpp
+++ user.cpp
@@ -22,6 +22,12 @@ string User::getName() {
 
 // Destructor.
 
+User::~User() {
+
+}
+
+// addGame().
+
 void User::addGame(vector<Game*> games){
 	string name;
 	int year;
@@ -31,74 +37,72 @@ void User::addGame(vector<Game*> games){
 	string status;
 	int serialNumber;
 	double price;
+	int option;
 	
-	cout<<"Ingrese el nombre del juego:"<<endl;
+	cout<<"Ingrese el nombre del juego: "<<endl;
 	getline(cin , name);
-	cout<<"Ingrese el ano:"<<endl;
+	cout<<"Ingrese el año: "<<endl;
 	cin>>year;
-	cout<<"Ingrese el numero de jugadores:"<<endl;
+	cout<<"Ingrese el número de jugadores: "<<endl;
 	cin>>numberPlayers;
-	cout<<"Ingrese el genero:"<<endl;
+	cout<<"Ingrese el género:"<<endl;
 	cin>>genre;
-	cout<<"Ingrese el estado del juego:"<<endl;
+	cout<<"Ingrese el estado del juego: "<<endl;
 	cin>>status;
 	status = 1000+games.size();
-	cout<<"Ingrese el precio:"<<endl;
+	cout<<"Ingrese el precio: "<<endl;
 	cin>>price;
 	//TODO Hacer metodo para consolas.
-	cout<<"Elija la desarrolladora de su juego:"<<endl;
-	cout<<"1.Microsoft \n 2.Sony \n 3.Nintendo \n 4.Bandai \n 5.Konami"<<endl;
-	cout<<"6.Square Enix.\n 7.Electronic \n Arts \n 8.SEGA \n 9.Ubisoft."<<endl;
-	int opcion; 
-	cin>>opcion;
-	switch(opcion){
+	cout<<"Elija la desarrolladora de su juego: "<<endl;
+	cout<<"1. Microsoft \n2. Sony \n3. Nintendo \n4. Bandai \n5. Konami"<<endl;
+	cout<<"6. Square Enix \n7. Electronic Arts \n8. SEGA \n9. Ubisoft \nOpción: "<<endl;
+	cin>>option;
+
+	switch(option){
 		case 1:
 			games.push_back(new microsoft(name, year, console, numberPlayers, genre, status, serialNumber, price));
-		break;
+			break;
 
 		case 2:
 			games.push_back(new sony(name, year, console, numberPlayers, genre, status, serialNumber, price));
-		break;
+			break;
 		
 		case 3:
 			games.push_back(new nintendo(name, year, console, numberPlayers, genre, status, serialNumber, price));	
-		break;
+			break;
 
 		case 4:
 			games.push_back(new Bandai(name, year, console, numberPlayers, genre, status, serialNumber, price));
-		break;
+			break;
 		
 		case 5:
 			games.push_back(new Konami(name, year, console, numberPlayers, genre, status, serialNumber, price));	
-		break;
+			break;
 
 		case 6:
 			games.push_back(new SquareEnix(name, year, console, numberPlayers, genre, status, serialNumber, price));
-		break;
+			break;
 
 		case 7:
 			games.push_back(new ElectronicArts(name, year, console, numberPlayers, genre, status, serialNumber, price));
-		break;
+			break;
 		
 		case 8:
 			games.push_back(new Sega(name, year, console, numberPlayers, genre, status, serialNumber, price));	
-		break;
+			break;
 
 		case 9:
 			games.push_back(new Ubisoft(name, year, console, numberPlayers, genre, status, serialNumber, price));
-		break;
+			break;
 
 		default:
-			cout<<"Opcion no valida , no se agregara ningun juego..."<<endl;
-		break;			
+			cout<<"Opción no válida. No se agregará ningún juego." << endl;
+			break;			
 	}
 }
 
+// addConsole().
+
 void User::addConsole(vector<Console*> consoles){
 	
-}
-
-
-User::~User() {
-
 }
